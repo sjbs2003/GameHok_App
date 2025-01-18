401923b0d2c1cc9cfcc9abebbc8fe0a553b05e0f
vicki <surajbarik2003@gmail.com>
2025-01-19 04:38:32 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kirab.gamehokapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kirab.gamehokapp"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
