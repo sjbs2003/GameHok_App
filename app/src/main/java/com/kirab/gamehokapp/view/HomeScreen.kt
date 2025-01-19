7691f32656e646de7a9c5f9d330ed01bdc11f5a6
vicki <surajbarik2003@gmail.com>
2025-01-20 00:32:48 +0530

    updated

--- app/src/main/java/com/kirab/gamehokapp/view/HomeScreen.kt
+++ app/src/main/java/com/kirab/gamehokapp/view/HomeScreen.kt
@@ -1,32 +1,17 @@
 package com.kirab.gamehokapp.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.kirab.gamehokapp.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
@@ -51,6 +36,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 item { GameSection(onViewAllClick = {}) }
                 item { TournamentSection(onViewAllClick = {}) }
                 item { CourseSection(onViewAllClick = {}) }
+                item { PeopleToFollowSection(onViewMoreClick = {}) }
             }
 
             BottomBar()
