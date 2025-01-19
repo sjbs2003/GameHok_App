882965cab3c33b5e5c149aad955e5684c6b6183f
vicki <surajbarik2003@gmail.com>
2025-01-20 00:48:19 +0530

    added coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // for image loading
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
